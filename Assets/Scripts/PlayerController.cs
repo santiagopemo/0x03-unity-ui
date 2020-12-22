2b0262da0cfe29258a3a7f4541c5d7f9a372e930
santiagopemo <santiagopemo@gmail.com>
2020-12-22 17:42:52 -0500

    added tasks 1 and 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 30;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rb.AddForce(movement * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
