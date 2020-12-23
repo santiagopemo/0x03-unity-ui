2595749031956788ea13626f8cef31ec5918f575
santiagopemo <santiagopemo@gmail.com>
2020-12-22 19:53:43 -0500

    added tasks 6 and 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Rigidbody rb;
     private int score;
+    public int health;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 30;
         score = 0;
+        health = 5;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -27,6 +29,11 @@ public class PlayerController : MonoBehaviour
             score++;
             other.gameObject.SetActive(false);
             Debug.Log("Score: " + score);
+        }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log("Health: " + health);
         }
     }
 
